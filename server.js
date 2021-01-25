9853cb40511c68c6d9a21cedce155f6604a3e87b
Steffny <steffnyb@gmail.com>
2021-01-25 11:33:57 +1000

    Prettified code

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-let express = require('express')
-let path = require('path')
-let app = express()
+let express = require("express");
+let path = require("path");
+let app = express();
 
 //use images & css files in static folder.
-app.use('/static', express.static(path.join(__dirname, 'static')))
- 
+app.use("/static", express.static(path.join(__dirname, "static")));
+
 //route - home
-app.get('/home', function(request, response){
-        response.sendFile(path.join(__dirname, 'adventure.html'))
-        
-	})
+app.get("/home", function (request, response) {
+  response.sendFile(path.join(__dirname, "adventure.html"));
+});
 
 //route - choice1
-app.get('/choice1', function(request, response){
-	response.sendFile(path.join(__dirname, 'choice1.html'))
-})
+app.get("/choice1", function (request, response) {
+  response.sendFile(path.join(__dirname, "choice1.html"));
+});
 
 //route - choice2
-app.get('/choice2', function(request, response){
-		response.sendFile(path.join(__dirname, 'choice2.html'))
-	})
+app.get("/choice2", function (request, response) {
+  response.sendFile(path.join(__dirname, "choice2.html"));
+});
 
 //route - conclusion
-app.get('/conclusion', function(request, response){
-	response.sendFile(path.join(__dirname, 'conclusion.html'))
-})
+app.get("/conclusion", function (request, response) {
+  response.sendFile(path.join(__dirname, "conclusion.html"));
+});
 
-app.listen(8080)
-console.log('Server is running at http://localhost:8080');
+app.listen(8080);
+console.log("Server is running at http://localhost:8080");
